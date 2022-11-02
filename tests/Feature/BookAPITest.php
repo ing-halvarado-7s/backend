b62ce7d33e08755bf707a211c827ce2baccfaea9
Halva <ing.halvarado.7s@gmail.com>
2022-11-02 19:43:39 -0400

    add: end test

--- tests/Feature/BookAPITest.php
+++ tests/Feature/BookAPITest.php
@@ -12,7 +12,7 @@ class BookAPITest extends TestCase
     
     /** @test */
     function can_get_all_books(){
-        $books = Book::factory(4)->cretae();
+        $books = Book::factory(4)->create();
 
         $response = $this->getJson(route('books.index'));
 
@@ -24,7 +24,62 @@ class BookAPITest extends TestCase
     }
 
     /** @test */
-    function can_create_books(){
+    function can_get_one_book(){
+        $book = Book::factory()->create();
 
+        $response = $this->getJson(route('books.show',$book));
+
+        $response->assertJsonFragment([
+            'title' => $book->title
+        ]);
     }
+
+    /** @test */
+    function can_create_books(){
+       $this->postJson(route('books.store'), [])
+            ->assertJsonValidationErrorFor('title');
+       
+        $this->postJson(route('books.store'),[
+            'title' => 'My New Book'
+        ])->assertJsonFragment([
+            'title' => 'My New Book'
+        ]);   
+        
+        $this->assertDatabaseHas('books', [
+            'title' => 'My New Book'
+        ]);
+        
+    }
+
+    /** @test */
+    function can_update_books(){
+        $book = Book::factory()->create();
+
+        $this->patchJson(route('books.update', $book), [])
+            ->assertJsonValidationErrorFor('title');
+        
+        $this->patchJson(route('books.update', $book), [
+                'title' => 'Edited Book'
+         ])->assertJsonFragment([
+                'title' => 'Edited Book'
+         ]);   
+         
+         $this->assertDatabaseHas('books', [
+                'title' => 'Edited Book'
+         ]);
+         
+     }
+
+     /** @test */
+    function can_delete_books(){
+        $book = Book::factory()->create();
+
+       
+        
+        $this->deleteJson(route('books.destroy', $book))
+            ->assertNoContent();   
+         
+         $this->assertDatabaseCount('books', 0);
+         
+     }
 }
